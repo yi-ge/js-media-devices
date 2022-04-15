88ca8183b87ee17d2e45a237190fa19f66bccfbd
yi-ge <a@wyr.me>
2022-04-15 21:35:33 +0800

    chore：rollup upgrade.

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,27 @@
 import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
-import minify from 'rollup-plugin-babel-minify'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { babel } from '@rollup/plugin-babel'
+import { terser } from "rollup-plugin-terser"
 
 export default {
   input: 'src/index.ts',
   output: {
     name: 'JsMediaDevices',
-    file: 'bin/js-media-devices.js',
+    file: 'bin/js-media-devices.min.js',
     format: 'umd',
     sourcemap: true,
-    sourcemapFile: 'bin/js-media-devices.js.map'
+    sourcemapFile: 'bin/js-media-devices.min.js.map'
   },
   plugins: [
     babel({
+      babelHelpers: 'runtime',
       exclude: 'node_modules/**',
-      runtimeHelpers: true,
       extensions: ['.js', '.ts']
     }),
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true
+    nodeResolve({
+      mainFields: ['jsnext:main', 'browser']
     }),
     commonjs(),
-    minify({
-      mangle: { topLevel: true }
-    })
+    terser()
   ]
 }
